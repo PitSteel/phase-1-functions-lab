59941d4be7b4e110b7b23b567972ee3f5a7d7edf
Ben Campbell <oakcityben@gmail.com>
2023-04-06 05:37:24 -0400

    message

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const headquarters = 42
+    return Math.abs(blocks - headquarters);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+
+}
+
+ function distanceTravelledInFeet(start, destination) {
+    let feet = Math.abs(start - destination);
+    return feet * 264
+ }
+
+ function calculatesFarePrice(start, destination) {
+    let feet = Math.abs((start - destination) * 264);
+    if (feet <= 400) {
+        return 0;
+    }
+        else if (feet > 400 && feet <= 2000) {
+        return Math.abs(feet - 400) * .02
+    }
+    else if (feet >= 2000 && feet <= 2500) {
+        return 25;
+    }
+    else if (feet > 2500); {
+    return 'cannot travel that far';
+}
+ }
